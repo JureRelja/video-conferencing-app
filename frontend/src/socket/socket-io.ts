c92bf3b7bac1ef4b855541fc82002aa8ce3a711e
JureRelja <jurereljanovic99@gmail.com>
2025-03-23 10:07:42 +0100

    fixed git

--- frontend/src/socket/socket-io.ts
+++ frontend/src/socket/socket-io.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(`${import.meta.env.BACKEND_URL}`, {});
+const socket = io(`${process.env.BACKEND_URL}`, {});
 
 socket.on('connect', () => {
   console.log('Connected to backend');
