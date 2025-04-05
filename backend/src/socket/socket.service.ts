3a4fc083decfe5d8a3a855a8daa0a3c005bf43f1
JureRelja <jurereljanovic99@gmail.com>
2025-04-05 12:28:35 +0200

    added signaling to accept transport data from client

--- backend/src/socket/socket.service.ts
+++ backend/src/socket/socket.service.ts
@@ -1,17 +1,105 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { WorkerService } from './worker.service';
-import { AppData, Router } from 'mediasoup/types';
+import { AppData, DtlsParameters, MediaKind, Router, RtpParameters, WebRtcTransport } from 'mediasoup/types';
 
 @Injectable()
 export class SocketService {
   constructor() {}
 
-  private readonly connectedClients: Map<string, { socket: Socket; router: Router<AppData> }> = new Map();
+  private readonly activeRooms: Map<string, Router<AppData>> = new Map();
+  private readonly connectedClients: Map<
+    Router<AppData>,
+    { producerTransport: null | WebRtcTransport<AppData>; consumerTransport: null | WebRtcTransport<AppData>; socketId: string }[]
+  > = new Map();
 
-  async handleConnection(socket: Socket) {
+  handleConnection(socket: Socket) {
     const clientId = socket.id;
+    console.log('Client connected', clientId);
 
+    socket.on('disconnect', () => {
+      console.log('Client disconected', clientId);
+    });
+
+    // Handle other events and messages from the client
+    socket.on('connect-transport', async ({ dtlsParameters, roomId }: { dtlsParameters: DtlsParameters; roomId: string }) => {
+      await this.connectTransport(socket, dtlsParameters, roomId);
+    });
+    socket.on('produce-transport', async ({ roomId, kind, rtpParameters }) => {
+      const producerId = await this.produceTransport(socket, roomId, kind, rtpParameters);
+
+      return { id: producerId };
+    });
+  }
+
+  async produceTransport(socket: Socket, roomId: string, kind: MediaKind, rtpParameters: RtpParameters) {
+    const router = this.activeRooms.get(roomId);
+
+    if (!router) {
+      console.error('Router not found for room:', roomId);
+      return;
+    }
+
+    const existingClients = this.connectedClients.get(router);
+
+    if (!existingClients) {
+      console.error('No clients connected to the router');
+      return;
+    }
+
+    const client = existingClients.find((client) => client.socketId === socket.id);
+
+    if (!client) {
+      console.error('Client not found in the connected clients');
+      return;
+    }
+
+    const transport = client.producerTransport;
+
+    if (!transport) {
+      console.error('Transport not found for the client');
+      return;
+    }
+
+    const producer = await transport.produce({ kind, rtpParameters });
+
+    return producer.id;
+  }
+
+  async connectTransport(socket: Socket, dtlsParameters: DtlsParameters, roomId: string) {
+    const router = this.activeRooms.get(roomId);
+
+    if (!router) {
+      console.error('Router not found for room:', roomId);
+      return;
+    }
+
+    const existingClients = this.connectedClients.get(router);
+
+    if (!existingClients) {
+      console.error('No clients connected to the router');
+      return;
+    }
+
+    const client = existingClients.find((client) => client.socketId === socket.id);
+
+    if (!client) {
+      console.error('Client not found in the connected clients');
+      return;
+    }
+
+    const transport = client.producerTransport;
+
+    if (!transport) {
+      console.error('Transport not found for the client');
+      return;
+    }
+
+    await transport.connect({ dtlsParameters });
+    console.log('Transport connected:', transport.id);
+  }
+
+  async createRouter(roomId: string, socketId: string) {
     if (!WorkerService.worker) {
       console.error('Worker is not initialized, initializing...');
       await WorkerService.createWorker();
@@ -35,25 +123,32 @@ export class SocketService {
       ],
     });
 
-    this.connectedClients.set(clientId, { socket, router });
-    console.log('Client connected', clientId);
+    this.activeRooms.set(roomId, router);
+    const existingClients = this.connectedClients.get(router);
 
-    socket.on('disconnect', () => {
-      this.connectedClients.delete(clientId);
-      console.log('Client disconected', clientId);
-    });
-
-    // Handle other events and messages from the client
+    if (existingClients) {
+      existingClients.push({ producerTransport: null, consumerTransport: null, socketId });
+    } else {
+      this.connectedClients.set(router, [{ producerTransport: null, consumerTransport: null, socketId }]);
+    }
   }
 
-  public getRouter(clientId: string) {
-    const client = this.connectedClients.get(clientId);
+  addTransports(router: Router<AppData>, socketId: string, producerTransport: WebRtcTransport<AppData>, consumerTransport: WebRtcTransport<AppData>) {
+    const existingClients = this.connectedClients.get(router);
+
+    if (!existingClients) return;
+
+    const client = existingClients.find((client) => client.socketId === socketId);
+
     if (client) {
-      console.log(`Router found for client ${clientId}`);
-      console.log('Router:', client.router.rtpCapabilities);
-      return JSON.stringify(client.router.rtpCapabilities);
+      client.producerTransport = producerTransport;
+      client.consumerTransport = consumerTransport;
     }
-    console.error(`No router found for client ${clientId}`);
+  }
+
+  public getRouter(roomId: string) {
+    const router = this.activeRooms.get(roomId);
+    return router;
   }
 
   // Add more methods for handling events, messages, etc.
