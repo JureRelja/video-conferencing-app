9f6cb618e8c9fbc0d90b74f561da7f9e4d826fc5
JureRelja <jurereljanovic99@gmail.com>
2025-04-17 19:13:16 +0200

    removed future chat functionality

--- frontend/src/app/rooms/[id]/page.tsx
+++ frontend/src/app/rooms/[id]/page.tsx
@@ -57,39 +57,6 @@ export default function Home() {
     getStream();
   }, []);
 
-  // Chat
-  // const [message, setMessage] = useState<string>('');
-  // const [messages, setMessages] = useState<Message[]>([]);
-  // const [chatHidden, setChatHidden] = useState<boolean>(true);
-
-  // const sendMessageHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault();
-
-  //   const messageForSending: Message = {
-  //     id: Date.now(),
-  //     message: message,
-  //     name: thisParticipant?.name as string,
-  //     socketId: thisParticipant?.socketId as string,
-  //     roomUUID: params.id as string,
-  //   };
-
-  //   webSocketsSignalling.emitMessage(messageForSending);
-
-  //   setMessages((prev) => [...prev, messageForSending]);
-
-  //   setMessage('');
-  // };
-
-  // const handleNewMessage = (message: Message) => {
-  //   setMessages((prev) => {
-  //     if (prev.find((msg) => msg.id === message.id)) {
-  //       return prev;
-  //     } else {
-  //       return [...prev, message];
-  //     }
-  //   });
-  // };
-
   return (
     <div className="flex flex-col gap-14 justify-center items-center w-full p-4">
       {/* Chat */}
@@ -111,58 +78,6 @@ export default function Home() {
             })}
         </div>
       )}
-
-      {/* <div className="flex flex-col gap-2 justify-center items-center w-full">
-        <h2 className="text-2xl text-center">Chat</h2>
-        <p className="text-gray-500 text-sm">Chat je trenutno u razvoju...</p>
-      {/* 
-      <div>
-      {!chatHidden ? (
-          <div className="flex gap-2 justify-center items-center">
-            <div className="flex flex-col justify-end items-end ">
-              <Button label="Sakrij chat ->" onClick={() => setChatHidden(true)} />
-            </div>
-            <div className="flex flex-col w-[400px] h-full py-2 border-2 border-gray-200">
-              <div className="h-[500px] flex flex-col gap-2 overflow-y-auto px-2">
-                {messages.map((message) => {
-                  return (
-                    <div
-                      key={message.id}
-                      className={`flex ${message.socketId === thisParticipant?.socketId ? 'justify-end' : 'justify-start'} items-center gap-2`}>
-                      <div className="flex flex-col">
-                        <p className={`underline ${message.socketId === thisParticipant?.socketId ? 'text-end' : 'text-start'}`}>
-                          {message.socketId === thisParticipant?.socketId ? 'Vi' : remoteParticipant?.name}
-                        </p>
-                        <p className=" border-2 border-gray-200 p-2 rounded-md">{message.message}</p>{' '}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-
-              <form className="flex justify-between gap-2 items-center p-2 border-t-2 border-gray-200">
-                <input
-                  className="border-2 px-3 py-[6px] rounded-md border-gray-400 w-full"
-                  type="text"
-                  placeholder="Poruka..."
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                />
-                <Button
-                  label="Send"
-                  onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
-                    sendMessageHandler(event);
-                  }}
-                />
-              </form>
-            </div>
-          </div>
-        ) : (
-          <div className="flex flex-col justify-end items-end ">
-            <Button label="<- Prikaži chat" onClick={() => setChatHidden(false)} />
-          </div>
-        )}
-      </div> */}
 
       {/* Invite */}
       <div className="flex items-center gap-2 justify-center w-fit">
