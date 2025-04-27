985559e447b1baf00485478acf6d30fa3ae22891
JureRelja <jurereljanovic99@gmail.com>
2025-04-27 09:15:05 +0200

    removed unused var

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { startCall, joinCall, roomExists } from '@/actions';
+import { startCall, roomExists } from '@/actions';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
